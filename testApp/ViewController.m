3cb02c53700f8ccb2dfa42e58335642072e811b7
Mohit R Pujara <mohitrpujara@mohits-mac-mini.local>
2016-01-20 13:42:56 +0530

    first test

--- testApp/ViewController.m
+++ testApp/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"Git Test");
+    NSLog(@"Git Test ttt");
 }
 
 - (void)didReceiveMemoryWarning {
