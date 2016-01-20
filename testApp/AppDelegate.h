1b9e0075c2882ad07179034e8d07ab339e24697d
Mohit R Pujara <mohitrpujara@mohits-mac-mini.local>
2016-01-20 13:38:33 +0530

    Initial Commit

---
+++ testApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Mohit R Pujara on 1/20/16.
+//  Copyright © 2016 Mohit R Pujara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Mohit R Pujara on 1/20/16.
+//  Copyright © 2016 Mohit R Pujara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
